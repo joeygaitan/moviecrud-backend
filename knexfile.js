bfdf2cc35bfa7d278e5546fdc3248f9f8e911c4f
Joey Gaitan <joeygaitan707@gmail.com>
2019-02-14 14:12:17 -0800

    changed connection in the knexfile.js to match the one in side learn with process.env.DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: 'postgres://localhost:5432/movies',
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: path.join(__dirname, 'db', 'migrations')
         },
